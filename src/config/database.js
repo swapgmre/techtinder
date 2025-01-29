b4917156b44680832ccfcaab0ff5257f7ad42d98
Swapnil Gamre <swapgmre@gmail.com>
2025-01-29 20:51:48 +0530

    updated db password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://swapgmre:K73u0cmmv4WFdEfd@techtinderproject.4lh4d.mongodb.net/tinderDB")
+  await mongoose.connect("mongodb+srv://swapgmre:FqyVzpYvBWPS407D@techtinderproject.4lh4d.mongodb.net/tinderDB")
 };
 
 module.exports = connectDB;
