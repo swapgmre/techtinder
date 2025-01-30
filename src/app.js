9b4d77362324cb2a9bca5b62f79b74378576a49d
Swapnil Gamre <swapgmre@gmail.com>
2025-01-30 20:55:15 +0530

    port changed

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ app.use("/", userRouter);
 
 connectDB().then(() => {
   console.log("Database connection established...");
-  app.listen(7777, () => {
-    console.log("Server is successfully listening on port 7777....")
+  app.listen(8000, () => {
+    console.log("Server is successfully listening on port 8000....")
   });
 }).catch((err) => {
   console.error("Database cannot be connected")
